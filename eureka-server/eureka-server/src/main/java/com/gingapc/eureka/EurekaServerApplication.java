78240b7f42d336faee585713f7c9c576026d99f1
Stiven63 <tucorreo@gmail.com>
2026-06-01 02:56:30 -0400

    feat: agregar eureka server

---
+++ eureka-server/eureka-server/src/main/java/com/gingapc/eureka/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.gingapc.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// @EnableEurekaServer activa el servidor de registro
+// este proyecto se tiene que correr PRIMERO antes que cualquier microservicio
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
